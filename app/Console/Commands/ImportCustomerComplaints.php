22a0ee09f852e4dcf19cac5f7a637551ef090939
wagena <wagena@domaintools.com>
2017-01-20 11:13:02 -0800

    first draft of import script

--- app/Console/Commands/ImportCustomerComplaints.php
+++ app/Console/Commands/ImportCustomerComplaints.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Keboola\Csv;
+use GuzzleHttp\Client;
+use DateTime;
 
 class ImportCustomerComplaints extends Command
 {
@@ -13,7 +15,7 @@ class ImportCustomerComplaints extends Command
      * @var string
      */
     protected $signature = 'complaints:import {file : relative patch to csv file from /storage directory}
-                                              {--throttle=200000 : microseconds to throttle between each document. default 200000.}  
+                                              {--throttle=300000 : microseconds to throttle between each document. default 300000.}  
                                               {--production} : send data to elasticsearch instance. dry run otherwise.';
 
     /**
@@ -24,12 +26,46 @@ class ImportCustomerComplaints extends Command
     protected $description = 'Import customer complaints from CSV to Elasticsearch instance';
 
     /**
+     * Guzzle client
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * Elasticsearch api uri
+     *
+     * @var string
+     */
+    protected $elasticsearchApi;
+
+    /**
+     * Elasticsearch user
+     *
+     * @var string
+     */
+    protected $elasticsearchUser;
+
+    /**
+     * Elasticsearch password
+     *
+     * @var string
+     */
+    protected $elasticsearchPassword;
+
+    /**
      * Create a new command instance.
      *
+     * @param Client $client
      * @return void
      */
-    public function __construct()
+    public function __construct(Client $client)
     {
+        $this->client = $client;
+        $this->elasticsearchApi = env('ELASTICSEARCH_API');
+        $this->elasticsearchUser = env('ELASTICSEARCH_USER');
+        $this->elasticsearchPassword = env('ELASTICSEARCH_PASSWORD');
+
         parent::__construct();
     }
 
@@ -43,8 +79,6 @@ class ImportCustomerComplaints extends Command
         $filePath = $this->argument('file');
         $throttle = $this->option('throttle');
         $production = $this->option('production');
-
-        $this->info("The file path is: {$filePath}");
 
         if(!file_exists($filePath)) {
             $this->error("File does not exist: {$filePath}");
@@ -80,9 +114,29 @@ class ImportCustomerComplaints extends Command
             return;
         }
 
-        // todo check if the document is already indexed to protect against overlapping runs. maybe output error.
-
-        // todo: post document to elasticsearch
+        $response = $this->client->request('POST', $this->elasticsearchApi . "consumer_complaints/complaint", [
+            'auth' => [$this->elasticsearchUser, $this->elasticsearchPassword],
+            'json' => [
+                "date_received" => DateTime::createFromFormat('m/d/Y', $row[0])->format('Y-m-d'),
+                "product" => $row[1],
+                "sub_product" => $row[2],
+                "issue" => $row[3],
+                "sub_issue" => $row[4],
+                "consumer_complaint_narrative" => $row[5],
+                "company_public_response" => $row[6],
+                "company" => $row[7],
+                "state" => $row[8],
+                "zip" => $row[9],
+                "tags" => $row[10],
+                "consumer_consent_provided" => $row[11],
+                "submitted_via" => $row[12],
+                "date_sent_to_company" => DateTime::createFromFormat('m/d/Y', $row[13])->format('Y-m-d'),
+                "company_response_to_consumer" => $row[14],
+                "timely_response" => $row[15],
+                "consumer_disputed" => $row[16],
+                "complaint_id" => $row[17]
+            ]
+        ]);
     }
 
     /**

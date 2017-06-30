19a0f49621eb5f43d67b318901a1ceb2bbbaf19f
wagena <wagena@domaintools.com>
2017-06-30 08:34:48 -0700

    update project name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'elasticsearch-demo',
+    'name' => 'Consumer Finance Complaints',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Consumer Finance Complaints') }}</title>
 
     <!-- Styles -->
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
